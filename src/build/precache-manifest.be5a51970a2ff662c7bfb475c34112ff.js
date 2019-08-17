7bec2160557be3e51ab5324fb651b7ab4cb14a90
Jiyang Xu <jiyangxu@Jiyangs-MacBook-Pro.local>
2019-08-17 17:37:41 +1000

    First commit

---
+++ src/build/precache-manifest.be5a51970a2ff662c7bfb475c34112ff.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fe353db27b96942a900a09832d13cbaf",
+    "url": "/index.html"
+  },
+  {
+    "revision": "bc20d489d6a856349a25",
+    "url": "/static/css/main.7fe9d009.chunk.css"
+  },
+  {
+    "revision": "67c5f44dd6d4ca27e50a",
+    "url": "/static/js/2.eb9286e2.chunk.js"
+  },
+  {
+    "revision": "bc20d489d6a856349a25",
+    "url": "/static/js/main.d006af75.chunk.js"
+  },
+  {
+    "revision": "590b1f1a44e44eae64fb",
+    "url": "/static/js/runtime~main.8f66630e.js"
+  }
+]);
